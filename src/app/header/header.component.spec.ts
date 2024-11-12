18b5a3a57f41b1872d624b7469bda13a90167159
Martin Echeverria <martin.echeverria@accenture.com>
2024-11-12 13:26:38 -0300

    Test for header.component

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,14 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { CvService } from '../services/cv.service';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { of } from 'rxjs';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let cvServiceMock: jasmine.SpyObj<CvService>;
 
   beforeEach(async () => {
+    // Mock del servicio CvService
+    cvServiceMock = jasmine.createSpyObj('CvService', ['getLinks']);
+    cvServiceMock.getLinks.and.returnValue(of({ links: [] })); // Devolver un observable vacío
+
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent]
+      imports: [MatToolbarModule, MatIconModule],  // Asegurarte de importar los módulos necesarios
+      declarations: [HeaderComponent],
+      providers: [
+        { provide: CvService, useValue: cvServiceMock } // Usar el mock para CvService
+      ]
     })
     .compileComponents();
 
@@ -19,5 +32,24 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should toggle menu', () => {
+    component.toggleMenu();
+    expect(component.isMenuOpen).toBeTrue();
+    component.toggleMenu();
+    expect(component.isMenuOpen).toBeFalse();
+  });
+
+  it('should call scrollToExperience when scrollToExperience is called', () => {
+    const scrollToExperienceSpy = spyOn(component, 'scrollToExperience');
+    component.scrollToExperience();
+    expect(scrollToExperienceSpy).toHaveBeenCalled();
+  });
+
+  it('should call scrollToStudies when scrollToStudies is called', () => {
+    const scrollToStudiesSpy = spyOn(component, 'scrollToStudies');
+    component.scrollToStudies();
+    expect(scrollToStudiesSpy).toHaveBeenCalled();
   });
 });
