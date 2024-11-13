2c7056d85a445871d6e1050bb7bc34bf4272c2a3
Martin Echeverria <martin.echeverria@accenture.com>
2024-11-13 11:50:20 -0300

    Change in environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: process.env['NG_APP_API_URL'] || 'https://cv-data-api.netlify.app/.netlify/functions/getCvData'
+    apiUrl: 'https://cv-data-api.netlify.app/.netlify/functions/getCvData'
   };
   
